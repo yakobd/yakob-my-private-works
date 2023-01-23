3626b2af072bfc079241f7f4c02022255080af1e
Yakob Dereje <yakobdereje.yd@gmail.com>
2023-01-22 17:30:05 -0800

    second commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+        public static void checkValue ( int x ){
+        String result = ( x == 2 )? " yes ": " no ";
+        System.out.println(result);
+    }
+        public static void main(String[] args) {
+            checkValue(2);
+        }
+}
